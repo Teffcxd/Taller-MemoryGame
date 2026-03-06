c936ac07f33c93967edbd07d676fdbc67cc94c1b
CesarRodriguez10 <alejorz2004@gmail.com>
2026-03-06 12:58:25 -0500

    Modelo de Carta

---
+++ app/src/main/java/modelo/Card.kt
@@ -0,0 +1,11 @@
+package modelo
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+class Card(
+    val id: Int
+) {
+    var isFaceUp by mutableStateOf(false)
+    var isMatched by mutableStateOf(false)
+}
