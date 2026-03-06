ff620b46c98685f6499ec1f7898ad07168dd7c6e
Jeimy Sanchez <jeimyestefanias@gmail.com>
2026-03-06 17:10:41 -0500

    logica para voltear cartas

--- app/src/main/java/viewmodel/GameViewModel.kt
+++ app/src/main/java/viewmodel/GameViewModel.kt
@@ -1,7 +1,9 @@
 package viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import model.Card
 
@@ -14,13 +16,12 @@ class GameViewModel : ViewModel() {
         private set
     private var firstCardIndex: Int? = null
     private var SecondCardIndex: Int? = null
-
     private var blockInput = false
 
     init{
         startGame()
     }
-    fun startGame(){
+    fun startGame(){ // funcion para mezclar
         val numbers = (1..8).toList()
         val shuffled = (numbers+numbers).shuffled()
         cards.clear()
@@ -33,5 +34,29 @@ class GameViewModel : ViewModel() {
         firstCardIndex=null
         SecondCardIndex=null
     }
+    fun flipCard(index: Int) {
 
+        if (blockInput) return
+
+        val card = cards[index]
+
+        if (card.isFaceUp || card.isMatched) return
+
+        card.isFaceUp = true
+
+        if (firstCardIndex == null) {
+
+            firstCardIndex = index
+
+        } else if (SecondCardIndex == null) {
+
+            SecondCardIndex = index
+            moves++
+
+            checkMatch() //crear la funcion para ver si hacen match
+        }
+    }
+    fun resetGame(){ //funcion para reiniciar juego
+        startGame()
+    }
 }
